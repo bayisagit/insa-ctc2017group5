28e0c838dd30fd9a3461d10915fb076911b9f0dd
Mekin Jemal <mekinjemal999@gmail.com>
2025-08-12 20:25:57 +0300

    refactor Prisma schema to unify enums and enhance model relationships

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -184,7 +184,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
       </SidebarHeader>
       <SidebarContent>
         <NavMain items={data.navMain} />
-        <NavProjects projects={data.projects} /> // Use NavProjects here
+        <NavProjects projects={data.projects} /> 
       </SidebarContent>
       <SidebarFooter>
         <NavUser user={data.user} />
